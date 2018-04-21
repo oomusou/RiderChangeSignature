9b3ace3a48bb315152171dcb761c07ea3b83514d
Oomusou <oomusou@gmail.com>
2018-04-21 23:49:17 +0800

    Change Signature 1

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 1);
+            
+            Console.WriteLine("{0}", result);
+        }
+    }
+}
